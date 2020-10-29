9862b9f558a5fbeb65434e19a0bc1ae633918e34
CinePlays <cineplays.business@gmail.com>
2020-10-29 22:36:08 +0100

    udated exports

--- lib/dsb_api.dart
+++ lib/dsb_api.dart
@@ -1,8 +1,9 @@
-export 'src/dsb_controller.dart';
+//models
 export 'src/models/school_class.dart';
 export 'src/models/subject.dart';
 export 'src/models/substitute.dart';
 export 'src/models/timetable_day.dart';
 export 'src/models/news.dart';
 export 'src/models/text_news.dart';
+//main
 export 'src/dsb_controller.dart';
