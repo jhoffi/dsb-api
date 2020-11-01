321d9b22dbb04ed2624f6b9a31d340d746c36d17
CinePlays <cineplays.business@gmail.com>
2020-11-01 18:49:55 +0100

    removed unneccessary schoolclass

--- lib/src/models/lesson.dart
+++ lib/src/models/lesson.dart
@@ -1,12 +1,11 @@
 import 'package:dsb_api/dsb_api.dart';
 
 class DSBLesson {
-  final DSBSchoolClass schoolClass;
   final List<int> lessons;
   final DSBSubject substitute;
   final DSBSubject subject;
   final String room;
   final String substituteText;
 
-  DSBLesson(this.schoolClass, this.lessons, this.substitute, this.subject, this.room, this.substituteText);
+  DSBLesson(this.lessons, this.substitute, this.subject, this.room, this.substituteText);
 }
