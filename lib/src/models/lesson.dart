3d427e6b06e39a6f2d56b3a8730bb1cc1f01dbe6
CinePlays <cineplays.business@gmail.com>
2020-11-03 04:05:58 +0100

    useless

--- lib/src/models/lesson.dart
+++ lib/src/models/lesson.dart
@@ -1,11 +1,11 @@
 import 'package:dsb_api/dsb_api.dart';
 
 class DSBLesson {
-  final List<int> lessons;
+  final int lesson;
   final DSBSubject substitute;
   final DSBSubject subject;
   final String room;
   final String substituteText;
 
-  DSBLesson(this.lessons, this.substitute, this.subject, this.room, this.substituteText);
+  DSBLesson(this.lesson, this.substitute, this.subject, this.room, this.substituteText);
 }
