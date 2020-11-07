ae62c0b7467bbcffb0fe2589e028ac64a674ca31
CinePlays <cineplays.business@gmail.com>
2020-11-07 12:40:23 +0100

    missing export

--- lib/dsb_api.dart
+++ lib/dsb_api.dart
@@ -5,5 +5,6 @@ export 'src/models/substitute.dart';
 export 'src/models/timetable_day.dart';
 export 'src/models/news.dart';
 export 'src/models/text_news.dart';
+export 'src/models/school_day.dart';
 //main
 export 'src/dsb_controller.dart';
